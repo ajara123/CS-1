3256ca29614f246390cff3078ff8ce6cf8f4fb79
ajara123 <30516941+ajara123@users.noreply.github.com>
2017-09-15 01:53:48 +1000

    Add files via upload

---
+++ 2.2 task.py
@@ -0,0 +1,22 @@
+#2.2 2 exercise
+name=input('Enter your name: ')
+print ('Hello', name)
+
+#2.2 3 exercise
+hours=input('Enter Hours ')
+rate=input('Enter Rate ' )
+print ('Pay:', float(hours)*float(rate))
+
+#2.2 4 exercise
+width=17
+height=12.0
+one=width//2
+two=width/2.0
+three=height/3
+four=1+2*5
+print (one, two, three, four)
+
+#2.2 5 exercise
+cels=input('Celsius ' )
+celsius=float(cels)*1.8+32
+print('Fahrenheit = ', celsius)
