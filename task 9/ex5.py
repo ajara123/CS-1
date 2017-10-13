7dbb45636d1fd9369aa5dedd569dcb19b86a5c4d
ajara123 <30516941+ajara123@users.noreply.github.com>
2017-10-14 00:13:13 +1100

    Update ex5.py

--- task 9/ex5.py
+++ task 9/ex5.py
@@ -1,4 +1,9 @@
 #Exercise 5
+
+#This program records the domain name (instead of the address) where the message was sent from instead
+#of who the mail came from (i.e., the whole email address). 
+#At the end of the program, print out the contents of your dictionary.
+
 fname=input('Enter a file name: ')
 fhand=open(fname)
 counts=dict()
