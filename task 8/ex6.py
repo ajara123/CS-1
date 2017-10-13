c665b3cf1760b6c8f7a1090dd6705235483eebe1
ajara123 <30516941+ajara123@users.noreply.github.com>
2017-10-14 00:07:24 +1100

    Update ex6.py

--- task 8/ex6.py
+++ task 8/ex6.py
@@ -1,5 +1,10 @@
 #Exercise 6
 
+#Rewrite the program that prompts the user for a list of numbers and prints out the maximum 
+#and minimum of the numbers at the end when the user enters "done". 
+#Write the program to store the numbers the user enters 
+#in a list and use the max() and min() functions to compute the maximum and minimum numbers after the loop completes.
+
 numbers=[]
 while True:
     NumInput=input('Enter a number: ')
